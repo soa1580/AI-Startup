a4461cb045d97a5e4faac3ad33855e1fbe718f3b
soa1580 <phamac17@yahoo.com>
2025-07-03 15:14:37 +0100

    add six

---
+++ src/main.js
@@ -0,0 +1,16 @@
+ function createThree (){
+    return 3
+ }
+
+ function createFour (){
+    return 4
+ }
+
+ function createFive (){
+    return 5
+ }
+ function createSix (){
+    return 6
+ }
+
+ module.exports = {createThree, createFour, createFive, createSix}
